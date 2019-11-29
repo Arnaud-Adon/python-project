97052be96d555593ffa749217172d033d228ff7f
Arnaud-Adon <assy.adon@yahoo.fr>
2019-11-29 11:18:30 +0000

    Connexion à la BDD

--- app.py
+++ app.py
@@ -1,42 +1,40 @@
 from flask import Flask, request, render_template, redirect, url_for
 import csv , pycurl , json
 from flask_sqlalchemy import SQLAlchemy
-import settings
+import config
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+"""
+Création d'une classe selon la syntaxe objet SQLAlchemy
+"""
+
+
+
+"""
+Instanciation de la base de donnée
+"""
 db = SQLAlchemy(app)
 
-# Création d'une classe selon la syntaxe objet SQLAlchemy
-class User(db.Model):
-    __tablename__ = 'user'
+class Tweet(db.Model):
+    """
+    Model Tweet
+    """
+    __tablename__ = 'Tweet'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64))
-    email = db.Column(db.String(100))
-    password = db.Column(db.String(100))
-    profile_picture = db.Column(db.String(100))
-    children = relationship("Tweet")
+    message = db.Column(db.String(300))
 
     # methode magique pour print()
     def __str__(self):
-        return "{} | {}".format(self.username, self.mail)
-
-class Tweet(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(300))
-    user_id = Column(Integer, ForeignKey('user.id'))
-
-    # methode magique pour print()
-    def __str__(self):
-        return "{} | {}".format(self.content, self.user_id)
+        return "{} | {}".format(self.username, self.message)
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="arnaudadon",
-    password="&Password",
-    hostname="arnaudadon.mysql.eu.pythonanywhere-services.com",
-    databasename="arnaudadon$gazouille",
+    username='arnaudadon',
+    password='&Password',
+    hostname='arnaudadon.mysql.eu.pythonanywhere-services.com',
+    databasename='arnaudadon$gazouille',
 )
 
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
@@ -44,108 +42,123 @@ app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.create_all()
 
-#engine = create_engine('mysql+mysqlconnector://arnaudadon:&Password@arnaudadon.mysql.eu.pythonanywhere-services.com/gazouille')
-#session = sessionmaker()
-#session.configure(bind=engine)
-#s = session()
-#Base.metadata.create_all(engine)
+def addMessage(d):
+    content = Tweet(
+        username=d["user-name"],
+        message=d["user-text"]
+    )
+    db.session.add(content)
+    db.session.commit()
 
-#engine = create_engine('mysql+mysqlconnector://' + MYSQL_DATABASE_USER + ':' + MYSQL_DATABASE_PASSWORD +
-#	'@' + MYSQL_DATABASE_HOST + '/' + MYSQL_DATABASE_DB)
-
-session = sessionmaker()
-session.configure(bind=engine)
-s = session()
+#def getMessages():
+#    mesTweets = Tweet.query.all()
+#   return mesTweets
 
 @app.route('/')
 def home():
-    return 'Bienvenue !' + MYSQL_DATABASE_USER
+    """
+    Page initiale de l'application
+    """
+    #return 'Bienvenue sur !'
+    return render_template('index.html')
 
 @app.route('/gaz', methods=['GET','POST'])
 def save_gazouille():
-	if request.method == 'POST':
-		print(request.form)
-		# send data to datatbase
-		dump_to_csv(request.form)
-		return redirect(url_for('timeline'))
-		#return "OK"
-	if request.method == 'GET':
-		return render_template('formulaire.html')
+    """
+    Fonction permettant d'afficher le formulaire ou d'enregitrer un tweet
+    """
+    if request.method == 'POST':
+        print(request.form)
+        addMessage(request.form)
+        return redirect(url_for('timeline_csv'))
+        #return "OK"
+    if request.method == 'GET':
+        return render_template('formulaire.html')
 
 @app.route('/timeline/csv', methods=['GET'])
 def timeline_csv():
-	gaz = parse_from_csv()
-	return render_template("timeline.html", gaz = gaz)
+    """
+    Lister les différents tweet
+    """
+    gaz = parse_from_csv()
+    return render_template("timeline.html", gaz = gaz)
 
 @app.route('/timeline/csv/<username>', methods=['GET'])
 def timeline_csv_user(username):
-	gaz = parse_from_csv_by_user(username)
-	return render_template("timeline.html", gaz = gaz)
+    """
+    Afficher la liste des tweet de l'auteur sélectionné
+    """
+    gaz = parse_from_csv_by_user(username)
+    return render_template("timeline.html", gaz = gaz)
 
+def parse_from_csv_by_user(username):
+    """
+    Parse le fichier gazouilles et affiche les différents tweet d'un utilisateur
+    """
+    gaz = []
+    with open('./gazouilles.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row[1]) <= 280 and row[0] == username:
+                gaz.append({"user":row[0], "text":row[1]})
+    return gaz
+
+def parse_from_csv():
+    """
+    Parse le fichier gazouilles et affiche les différents tweet
+    """
+    gaz = []
+    with open('./gazouilles.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if len(row[1]) <= 280:
+                gaz.append({"user":row[0], "text":row[1]})
+    return gaz
+
+def dump_to_csv(d):
+    """
+    Insert les données du formulaire dans le fichier gazouilles.csv
+    """
+    donnees = [d["user-name"],d["user-text"] ]
+    with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(donnees)
 
 @app.route('/timeline', methods=['GET'])
 def timeline():
-	allTweet = getAllTweet()
-	return render_template("timeline.html", allTweet = allTweet)
-
+    """
+    Affiche tous les tweets de la base de donnée
+    """
+    allTweet = getAllTweet()
+    return render_template("timeline.html", allTweet = allTweet)
 
 def getAllTweet():
-	allTweet = []
-	for p in s.query(Form).all():
-    	allTweet.append(p)
+    """
+    Recherche tous les tweet en base de donnée
+    """
+    tweetFiltered = []
+    allTweet = Tweet.query.all()
+    for tweet in allTweet:
+        if(len(tweet.message) <= 280):
+            tweetFiltered.append(tweet)
+    return tweetFiltered
 
-    return allTweet
 
 @app.route('/timeline/<username>', methods=['GET'])
 def timeline_by_user(username):
-	allTweetByUser = getTweetByUser(username)
-	return render_template("timeline.html", allTweet = allTweetByUser)
+    """
+    Fonction qui affiche tous les tweet d'un utilisateur choisie
+    """
+    allTweetByUser = getTweetByUser(username)
+    return render_template("timeline.html", allTweet = allTweetByUser)
 
 def getTweetByUser(username):
-	allTweetByUser = []
-	for p in s.query(Form).filter_by(username=username & len(message) <= 280):
-    	allTweetByUser.append(p)
-
-    return allTweetByUser
-
-
-
-def parse_from_csv_by_user(username):
-	gaz = []
-	with open('./gazouilles.csv', 'r') as f:
-		reader = csv.reader(f)
-		for row in reader:
-			if len(row[1] <= 280 & row[0] == 'username')
-				gaz.append({"user":row[0], "text":row[1]})
-	return gaz
-
-def parse_from_csv():
-	gaz = []
-	with open('./gazouilles.csv', 'r') as f:
-		reader = csv.reader(f)
-		for row in reader:
-			if len(row[1] <= 280)
-				gaz.append({"user":row[0], "text":row[1]})
-	return gaz
-
-def dump_to_csv(d):
-	donnees = [d["user-name"],d["user-text"] ]
-	with open('./gazouilles.csv', 'a', newline='', encoding='utf-8') as f:
-		writer = csv.writer(f)
-		writer.writerow(donnees)
-
-
-@app.route('/timeline/delete/<int:id>')
-def timeline_delete_tweet(id):
-	s.query(Tweet).filter_by(id=id).delete()
-	s.commit()
-
-	return redirect(url_for('timeline'))
-
-
-@app.route('/timeline/add' , method = ['POST'])
-def timeline_add_tweet():
-	tweet = Tweet(title="depuis SQLAlchemy", message="message",
-	            gender=False, like=2)
-	s.add(form)
-	s.commit()
+    """
+    Fonction qui recherche en base de donnée les tweet d'un utilisateur choisie
+    """
+    tweetFiltered = []
+    allTweetByUser = Tweet.query.filter(Tweet.username==username)
+    for tweetByUser in allTweetByUser:
+        if(len(tweetByUser.message) <= 280):
+            tweetFiltered.append(tweetByUser)
+    return tweetFiltered
